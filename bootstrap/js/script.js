7eb92d0258eb822e8246a94072263e91d5782e38
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-01-20 13:41:54 +0600

    Add files via upload

---
+++ bootstrap/js/script.js
@@ -0,0 +1,24 @@
+// menubar hover active
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('section[id]');
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if (scrollY >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.href.includes(current)) {
+        link.classList.add('active');
+      }
+    });
+  });
+});
